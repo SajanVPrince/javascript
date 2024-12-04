bdf93181506f22b7a86c54c3e5cc2fb36a6280e6
SajanVPrince <sajanvprince@gmail.com>
2024-12-04 09:30:30 +0530

    Javascript beginning

---
+++ demo.js
@@ -0,0 +1,25 @@
+// let a=10
+// a=20
+// var b=10
+// b=30
+// const c=10
+// c=20
+// console.log(a,b,c);
+// alert("welcome")
+
+// let a=1.2
+// console.log(typeof a);
+
+// console.log(10+1);
+// console.log(10-1);
+// console.log(10*1);
+// console.log(10/1);
+// console.log(10**1);
+// a=10
+// a++
+// b=10
+// b--
+// console.log(a,b);
+
+// console.log(10==10 && 10==100);
+// console.log(10==100 || 10==100);
