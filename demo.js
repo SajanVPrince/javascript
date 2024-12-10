2052628135ea1de32139924960d7d2c2ef7779be
SajanVPrince <sajanvprince@gmail.com>
2024-12-10 18:26:32 +0530

    Methods

--- demo.js
+++ demo.js
@@ -41,3 +41,67 @@
     
 // }
 
+// for(let i=0;i<=10;i++){
+//     console.log(i);   
+// }
+
+// let i=0
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// let i=0
+// do{
+//     console.log(i);
+//     i++ 
+// }
+// while(i<=10)
+
+// let l=[1,2,3,4,5]
+// let l ={name:'sjan',age:22}
+// for (i in l){
+//     console.log(i);
+// }
+
+// for (i of l){
+//     console.log(i);
+// }
+
+// let a='welcome'
+// console.log(a.length);
+// console.log(a.charAt(5));
+// console.log(a.concat(' ','sajan'));
+// console.log(a.endsWith(e));
+// console.log(a.startsWith(w));
+// console.log(a.slice(0,4));
+// console.log(a.trim());
+// console.log(a.trimStart());
+// console.log(a.trimEnd());
+
+// l=[1,2,3,4,5]
+// l.pop()
+// console.log(l.length);
+// console.log(l.at(2));
+// l.push(6,7)
+// console.log(l);
+
+// l.shift()
+// l.unshift(10)
+// l[1]=100
+// console.log(l);
+
+function sample(){
+    console.log('sample1');
+}
+sample()
+
+let sample1=function(){
+    console.log('sample1');
+}
+sample1()
+
+let sample2=()=>{
+    console.log('sample2');
+}
+sample2()
--- work/task/task.js
+++ work/task/task.js
@@ -5,10 +5,10 @@ function bonus(){
     if (e>=5){
         let a=s*5/100
         let ts=a+s
-        res.innerHTML=ts
+        res.innerHTML=`this Month Salary =${ts}`
     }      
     else{
-        res.innerHTML='No Change'     
+        res.innerHTML='No Change in salary'     
     }
 }
 function elebill(){
@@ -16,18 +16,18 @@ function elebill(){
     let div=document.getElementById("div")
 
     if (a<=100){
-        div.innerHTML=0
+        div.innerHTML=`Your Bill Amount is 0`
     }
     else{
         if (a<=200 && a>100){
             let up=a-100
             let ua=up*5
-            div.innerHTML=ua
+            div.innerHTML=`Your Bill amount is =${ua}`
         }
         else{
             let ui=a-200
             let us=ui*10+500
-            div.innerHTML=us
+            div.innerHTML=`Your Bill amount is =${us}`
         }
     }
 }
@@ -67,4 +67,95 @@ function city(){
     }
         
 }
+function week(){
+    let a=parseInt(document.getElementById("week").value)
+    let dis=document.getElementById("dis1")
+    if (a===1){
+        dis.innerHTML="Sunday"
+    }   
+    else if (a===2){
+        dis.innerHTML="Monday"        
+    }
+    else if (a===3){
+        dis.innerHTML="Tuesday"
+    }
+    else if (a===4){
+        dis.innerHTML="Wednesday"
+    }
+    else if (a===5){
+        dis.innerHTML="Thursday"
+    }
+    else if (a===6){
+        dis.innerHTML="Friday"
+    }
+    else if (a===7){
+        dis.innerHTML="Saturday"
+    }
+    else{
+        dis.innerHTML="Invalid"
+    }
+}
+function prdbill(){
+    let a=parseInt(document.getElementById("bill1").value)
+    let dis2=document.getElementById("dis2")
+    let dis3=document.getElementById("dis3")
 
+    if (a>100000){
+        b=a*15/100
+        total=a+b
+        dis2.innerHTML=`Ex-Showroom Price = ${a} and tax amount is = ${b}`
+        dis3.innerHTML=`total amount=${total}`
+
+    }
+    else if (a>50000 && a<=100000){
+        b=a*10/100
+        total=a+b
+        dis2.innerHTML=`Ex-Showroom Price = ${a} and tax amount is = ${b}`
+        dis3.innerHTML=`total amount=${total}`
+    }
+    else{
+        b=a*5/100
+        total=a+b
+         dis2.innerHTML=`Ex-Showroom Price = ${a} and tax amount is = ${b}`
+        dis3.innerHTML=`total amount=${total}`
+    }
+}
+
+function odd(){
+    console.clear()
+    let a=parseInt(document.getElementById('fnum').value)
+    let b=parseInt(document.getElementById('lnum').value)
+    for(let i=a;i<=b;i++){
+        if(i%2!==0){
+            console.log(i);  
+        }
+    }
+}
+function even(){
+    console.clear()
+    let a=parseInt(document.getElementById('fnum').value)
+    let b=parseInt(document.getElementById('lnum').value)
+    for(let i=a;i<=b;i++){
+        if(i%2===0){
+            console.log(i);  
+        }
+    }
+}
+function mul(){
+    let a=parseInt(document.getElementById('fnum1').value)
+    let b=parseInt(document.getElementById('lnum1').value)
+    for(let i=1;i<=b;i++){
+        console.log(`${a}*${i}=${a*i}`);
+    }
+}
+
+// function rev() {
+//     let a = document.getElementById('fnum1').value;  
+//     i=0
+//     let l = a.length;
+//     b=a.split("").reverse().join("")
+//     while (i<l){
+//         console.log(b[i]);
+//         i+=1
+//     }
+// }
